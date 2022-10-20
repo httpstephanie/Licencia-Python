8d855a5f034eb4c9a6a3143b53a8d1cb7bebf460
httpstephanie <110868588+httpstephanie@users.noreply.github.com>
2022-10-20 12:52:58 -0500

    Add files via upload

---
+++ estructuracondicional.py
@@ -0,0 +1,9 @@
+edad = 16
+tiene_licencia = False
+ 
+if edad >= 18 and tiene_licencia:
+    print("Puedes conducir")
+elif edad < 18:
+    print("No puedes conducir aún. Debes tener 18 años y contar con una licencia")
+else:
+    print("No puedes conducir. Necesitas contar con una licencia")
